0342e5b676d77560658f9e78938c2b4860363ace
Lai 'Lain' Jiang <lain.lai.jiang@gmail.com>
2015-10-09 18:51:20 -0500

    (mobile) in progress to add image picker

--- HabitTrainer/HabitSummary.js
+++ HabitTrainer/HabitSummary.js
@@ -1,22 +1,63 @@
 'use strict';
 
 var React = require('react-native');
+var helpers = require('./helper/helpers.js');
+var UIImagePickerManager = require('NativeModules').UIImagePickerManager;
 
 var {
   StyleSheet,
   View,
   Text,
-  Component
+  Component,
+  Image
 } = React;
 
+var HABITS = [
+  {habitName: 'Submit a Pull Request', streak: 5, checkinCount: 25, failedCount: 3, reminderTime: '2:30 PM', dueTime: '4:30 PM', streakRecord: 15, active:true},
+  {habitName: 'Complete a Pomodoro', streak: 10, checkinCount: 20, failedCount: 4, reminderTime: '2:30 PM', dueTime: '8:30 PM', streakRecord: 20, active:true},
+  {habitName: 'Workout', streak: 8, checkinCount: 15, failedCount: 2, reminderTime: '2:30 PM', dueTime: '4:30 PM', streakRecord: 8, active:true}
+];
+
+var user = {
+  name: 'Pied Piper',
+  dateJoined: '10/06/15'
+}
+
+var date = new Date();
+var hour = date.getHours();
+var min = date.getMinutes();
 
 var HabitSummary = React.createClass ({
   render: function(){
   return (
     <View style={styles.container}>
-      <Text style={styles.content}>
-        Test -- display on home
-      </Text>
+      <View style={{flexDirection: 'row'}}>
+        <Image
+        style={styles.icon}
+        source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+        />
+        <View>
+          <Text style={styles.content}>
+            Hello, {user.name}! 
+          </Text>
+          <Text style={styles.contentSmall}>
+            Training since {user.dateJoined}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <Text style={styles.content}>
+          Next Up
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.content}>
+          {helpers.nextHabit(HABITS)}
+        </Text>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
@@ -29,10 +70,21 @@ var styles = StyleSheet.create({
     alignItems: 'center',
   },
   content: {
+    // borderWidth: 1,
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  }
+  },
+  contentSmall: {
+    // borderWidth: 1,
+    fontSize: 12,
+    textAlign: 'center',
+    margin: 10,
+  },
+  icon: {
+    width: 75,
+    height: 75,
+  },
 });
 
 module.exports = HabitSummary;
--- HabitTrainer/helper/helpers.js
+++ HabitTrainer/helper/helpers.js
@@ -2,16 +2,25 @@
 
 var nextHabit = exports.nextHabit = function(habits) {
   var date = new Date();
-  var hr = date.getHours();
-  var min = date.getMinutes();
+  var current = convertTime(date.getHours(),  date.getMinutes());
+  var diff, next, temp;
   for (var i = 0; i < habits.length; i++) {
-    parseTime();
+    var due = parseTime(habits[i].dueTime, convertTime);
+    temp = due - current;
+    if (temp > 0) {
+      if (temp < diff || diff === undefined) {
+        diff = temp;
+        next = habits[i].habitName;
+      }
+    }
   }
+  next = next || 'good job!'
+  return next;
 }
 
 var parseTime = exports.parseTime = function(time, cb) {
   var hr = time.match(/\d+(?=:)/gi).join("")*1;
-  var min = time.match(/\d+(?=[ap]m)/gi).join("")*1;
+  var min = time.match(/\d+(?= [ap]m)/gi).join("")*1;
   var ap = time.match(/AM|PM/).join("");
 
   return cb(hr, min, ap);
@@ -24,12 +33,21 @@ var convertTime = exports.convertTime = function(hr, min, ap) {
     }
     return hr * 12 + min;
   }
-  else {
+  else if (ap === 'PM'){
     if (hr !== 12) {
       return (hr + 12) * 12 + min;
     }
     return hr * 12 + min;
   }
+  else {
+    return hr * 12 + min;
+  }
 }
 
-console.log(parseTime('4:30PM', convertTime));
+var HABITS = [
+  {habitName: 'Submit a Pull Request', streak: 5, checkinCount: 25, failedCount: 3, reminderTime: '2:30 PM', dueTime: '4:30 PM', streakRecord: 15, active:true},
+  {habitName: 'Complete a Pomodoro', streak: 10, checkinCount: 20, failedCount: 4, reminderTime: '2:30 PM', dueTime: '4:30 PM', streakRecord: 20, active:true},
+  {habitName: 'Workout', streak: 8, checkinCount: 15, failedCount: 2, reminderTime: '2:30 PM', dueTime: '4:30 PM', streakRecord: 8, active:true}
+];
+
+console.log(nextHabit(HABITS));
