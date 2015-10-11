9ceb3bfc3626510d9e550b7bf135b1bc13adf418
melindab <melindab@users.noreply.github.com>
2015-10-10 19:04:25 -0500

    Minor changes.

--- server/middlewares/emailer.js
+++ server/middlewares/emailer.js
@@ -4,23 +4,25 @@ var transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
     user: 'habittrainersummary@gmail.com',
-    password: 'habittrainerpassword'
+    pass: 'habittrainerpassword'
   }
 });
 
-var mailOptions = {
-  from: '"Habit Trainer" <habittrainersummary@gmail.com>',
-  to: user,
-  subject: 'Weekly Habits Summary',
-  text: 'This is a reminder to complete your habits.',
-  html: '<b>This is a reminder to complete your habits.</b>'
-};
-
-module.exports.sendSummaryEmail = function(user) {
-  transporter.sendMail(mailOptions, function(error, info) {
+ var sendSummaryEmail = function(user) {
+  transporter.sendMail({
+    from: 'habittrainersummary@gmail.com',
+    to: user,
+    subject: 'Weekly Habits Summary',
+    text: 'This is a reminder to complete your habits.',
+    html: '<b>This is a reminder to complete your habits.</b>'
+  }, function(error) {
     if (error) {
       return console.log(error);
     }
-    console.log('Summary email sent: ' + info.response);
+    console.log('Summary email sent');
   });
 };
+
+// sendSummaryEmail('habittrainersummary@gmail.com');
+
+module.exports.sendSummaryEmail = sendSummaryEmail;
